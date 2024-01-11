bbdffc928701f78aba356d0d585d6b74b1e84d9d
Pauli Ann Bumanlag <pro.pau03@gmail.com>
2024-01-11 16:14:09 +0800

    webpack

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>I am React App!</h1>, document.getElementById('electronChat'));
+/* --------------------------------------------------------------------------------
+ReactDOM.render is no longer supported in React 18. Use createRoot instead.
+Until you switch to the new API, your app will behave as if it’s running React 17.
+Learn more: https://reactjs.org/link/switch-to-createroot
+-------------------------------------------------------------------------------- */
+// ReactDOM.render(<h1>I am React App!</h1>, document.getElementById('electronChat'));
+
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('electronChat');
+const root = createRoot(container);
+root.render(<h1>I am React App!</h1>);
