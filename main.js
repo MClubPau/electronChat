800405af6978b52ce3c095d82207be3b97f0f834
Pauli Ann Bumanlag <pro.pau03@gmail.com>
2024-01-11 14:34:42 +0800

    node integration

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Main Window
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow ,Notification} = require('electron');
+const path = require('node:path');
 
 const createWindow = () => {
     // Browser Window <- Renderer Process
@@ -18,17 +19,28 @@ const createWindow = () => {
 }
 
 app.whenReady().then(() => {
-    createWindow()
+    createWindow();
+
+    const notification = new Notification({title: 'Hello World', body: 'My test message'});
+    notification.show();
+
+    const parsed = path.parse('/home/user/dir/file.txt');
+    console.log(parsed.base);
+    console.log(parsed.ext);
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
+            createWindow();
         }
     })
 })
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
 })
+
+// Chromium -> web engine for rendering UI, full Chrome-like web browser
+// V8 -> engine that provides capabilities to execute, run, JS code in the browser
+// Node JS(V8) -> we are able to run JS code + provides more features
