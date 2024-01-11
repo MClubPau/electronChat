08eeb7b512ce6e2e54e597e05762955e71f87b76
Pauli Ann Bumanlag <pro.pau03@gmail.com>
2024-01-11 13:24:12 +0800

    update

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
+// Main Window
 const { app, BrowserWindow } = require('electron/main')
 
 const createWindow = () => {
+    // Browser Window <- Renderer Process
     const win = new BrowserWindow({
-        width: 800,
-        height: 600
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
     })
 
     win.loadFile('index.html');
+
+    // Open the DevTools initially (optional)
+    win.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
