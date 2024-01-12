0b62bede8d71d103a3bf31cbee14a352b4b42fd9
Pauli Ann Bumanlag <pro.pau03@gmail.com>
2024-01-12 14:44:28 +0800

    separation to components

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 1200,
         height: 800,
+        backgroundColor: "white",
         webPreferences: {
             nodeIntegration: false,
             worldSafeExecuteJavaScript: true,
