2f0093c5fd5c90e08e8b1baa823395faf6e2b125
pauliannbumanlag <90313028+pauliannbumanlag@users.noreply.github.com>
2024-01-16 16:13:46 +0800

    dotenv

--- src/js/db/firestore.js
+++ src/js/db/firestore.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const config = {
-  apiKey: "AIzaSyBKici-dKXHoc15LmEGMUVY1bqBzj2p_BE",
-  authDomain: "electron-chat-d519e.firebaseapp.com",
-  projectId: "electron-chat-d519e",
-  storageBucket: "electron-chat-d519e.appspot.com",
-  messagingSenderId: "451449104527",
-  appId: "1:451449104527:web:dc986c4aafb1318f8068fa",
-  measurementId: "G-H6TQ824RJ7"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 export default firebase.initializeApp(config).firestore();
